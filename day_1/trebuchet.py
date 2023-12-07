fea6d478b3c7534417f59195210ae2d089e9f199
Ahmed Iqbal <ahmed.iqbal@ucalgary.ca>
2023-12-07 10:07:27 -0700

    correct solution

--- day_1/trebuchet.py
+++ day_1/trebuchet.py
@@ -1,32 +1,42 @@
 '''Solution to Advent of Code day 1'''
 
-
 # Step 1: Read in the input file as a list
 def read_text_file(file):
-    input_file = open(file, "r")
-    data = input_file.read()
+    data_list = []
 
-    return data
+    with open(file, "r") as f:
+        for line in f:
+            data_list.extend(line.split())
 
-# reverse the word
-def reverse_slicing(s):
-    return s[::-1]
+    return data_list
 
-# Step 2: Define logic to calculate calibration value for each word
-def calculate_calibration_value(word):
-    # go from left to right to find first numeric value using isnumeric()
+def calculate_calibration_value_first_digit(word):
     calibration_value = 0
-    for w in word: # go from right to left to find first numeric value using isnumeric()
+    for w in word: # go from right to left to find first numeric value using isnumeric(). 
+                   # this needs to be multiplied by 10 since its the first figit
         if w.isnumeric():
-            calibration_value += int(w)
+            calibration_value += (int(w)*10)
             break
 
-    for w in reverse_slicing(word):
+    return calibration_value
+
+def calculate_calibration_value_second_digit(word):
+    calibration_value = 0
+    reversed_word = reversed(word)
+
+    for w in reversed_word:
         if w.isnumeric():
             calibration_value += int(w)
             break
 
     return calibration_value
+
+# Step 2: Define logic to calculate calibration value for each word
+def calculate_calibration_value(word):
+    first_digit = calculate_calibration_value_first_digit(word)
+    second_digit = calculate_calibration_value_second_digit(word)
+
+    return first_digit+second_digit
 
 # Step 3: Iterate over list of input words and calculate calibration values
 def summarize_calibration_values(file):
@@ -41,4 +51,4 @@ def summarize_calibration_values(file):
 if __name__ == "__main__":
     text_file = 'input.txt'
     total = summarize_calibration_values(text_file)
-    print("The total is: ", total) # total is 21560 based on this program
+    print("The total is: ", total) # answer is 53651
