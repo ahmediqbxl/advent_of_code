403c119c6891223f44056189cc605a978c7993ee
Ahmed Iqbal <ahmed.iqbal@ucalgary.ca>
2023-12-10 14:46:23 -0700

    part two

--- day_1/trebuchet_part_two.py
+++ day_1/trebuchet_part_two.py
@@ -1,4 +1,19 @@
-'''Solution to Advent of Code day 1 Part 1'''
+'''Solution to Advent of Code day 1 Part 2'''
+
+import re
+
+word_to_digit = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'zero': '0'
+}
 
 # Step 1: Read in the input file as a list
 def read_text_file(file):
@@ -7,9 +22,26 @@ def read_text_file(file):
     with open(file, "r") as f:
         for line in f:
             data_list.extend(line.split())
-
     return data_list
 
+def is_key_in_string(dictionary, input_string):
+    for key in dictionary.keys():
+        if input_string.startswith(key):
+            return True
+    return False
+
+def replace_keys_with_values(dictionary, input_string):
+    for key, value in dictionary.items():
+        if key in input_string:
+            input_string = input_string.replace(key, str(value))
+    return input_string
+
+# Step 2: Replace "numeric words" with actual digits
+def standarize_word(text):
+    if is_key_in_string(word_to_digit, text):
+        text = replace_keys_with_values(word_to_digit, text)
+    return text
+    
 def calculate_calibration_value_first_digit(word):
     calibration_value = 0
     for w in word: # go from right to left to find first numeric value using isnumeric(). 
@@ -17,7 +49,6 @@ def calculate_calibration_value_first_digit(word):
         if w.isnumeric():
             calibration_value += (int(w)*10)
             break
-
     return calibration_value
 
 def calculate_calibration_value_second_digit(word):
@@ -28,13 +59,13 @@ def calculate_calibration_value_second_digit(word):
         if w.isnumeric():
             calibration_value += int(w)
             break
-
     return calibration_value
 
-# Step 2: Define logic to calculate calibration value for each word
+# Step 3: Define logic to calculate calibration value for each word
 def calculate_calibration_value(word):
     first_digit = calculate_calibration_value_first_digit(word)
     second_digit = calculate_calibration_value_second_digit(word)
+    print(first_digit, " | ", second_digit)
 
     return first_digit+second_digit
 
@@ -43,7 +74,9 @@ def summarize_calibration_values(file):
     inputs = read_text_file(file) # list of inputs
     total = 0 
     for word in inputs:
-        calibration_value = calculate_calibration_value(word)
+        standarized_word = standarize_word(word)
+        print(standarized_word)
+        calibration_value = calculate_calibration_value(standarized_word)
         total += calibration_value
 
     return total
@@ -51,4 +84,4 @@ def summarize_calibration_values(file):
 if __name__ == "__main__":
     text_file = 'input.txt'
     total = summarize_calibration_values(text_file)
-    print("The total is: ", total) # answer is 53651
+    print("The total is: ", total) # answer is 281
